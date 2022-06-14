aaebb0f4b3651ef7dd1c5663d5a605186a35f0f8
David Wiens <davidmwiens@gmail.com>
2022-06-14 06:19:37 -0700

    Improve point geo and robot

--- geometry.py
+++ geometry.py
@@ -1,7 +1,15 @@
-
+from math import *
 
 class Point:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
+
+def hypotenuse(pt1: Point, pt2: Point):
+    return sqrt(((pt1.x - pt2.x)**2) + ((pt1.y - pt2.y)**2))
--- robot.py
+++ robot.py
@@ -1,17 +1,50 @@
+from ctypes import pointer
 from math import sin, cos, pi, radians, degrees
+
+import geometry as geo
 
 LINK_A_LEN = 300
 LINK_B_LEN = 200
 
 
-
 def getPositionsFromTime(t):
-    pA = 1
-    pB = 1.8
-    angleADEG = 20 + 10*sin(2*pi/pA*t)
-    angleBDEG = 60 + 30*sin(2*pi/pB*t)
+    periodA = 1
+    periodB = 1.8
+    angleADEG = 20 + 10*sin(2*pi/periodA*t)
+    angleBDEG = 60 + 30*sin(2*pi/periodB*t)
     
     return (angleADEG, angleBDEG)
+
+
+# If there are N links, there will be N+1 joint points
+# Cartesian coordinates, not display coordinates
+def getJointPoints(t):
+    
+    # Add origin
+    points = []
+    points.append(geo.Point(0, 0))
+    current = points[-1]
+    
+    (angleADEG, angleBDEG) = getPositionsFromTime(t)
+
+    # End of link A
+    next = geo.Point()
+    angle += angleADEG
+    next.x = current.x + LINK_A_LEN*cos(radians(angle))
+    next.y = current.y + LINK_A_LEN*sin(radians(angle))
+    points.append(next)
+    current = points[-1]
+
+    # End of link B
+    next = geo.Point()
+    angle += angleBDEG
+    next.x = current.x + LINK_B_LEN*cos(radians(angle))
+    next.y = current.y + LINK_B_LEN*sin(radians(angle))
+    points.append(next)
+    current = points[-1]
+
+    return points
+
 
 
 
